4f7e70a0772c8d939e3fb26f8ec7b57d26a10fe5
Laima Urnezaite <laima.urnezaite@gmail.com>
2020-05-27 23:21:16 +0300

    fix login/logout redirect urls

--- dogfacts_project/dogfacts_project/settings.py
+++ dogfacts_project/dogfacts_project/settings.py
@@ -123,5 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'homepage'
+LOGOUT_REDIRECT_URL = 'homepage'
