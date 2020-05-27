49316404a31eaee0d12f285e8c82039db1e2a856
Laima Urnezaite <laima.urnezaite@gmail.com>
2020-05-27 23:20:35 +0300

    create home and addFact views

--- dogfacts_project/facts/views.py
+++ dogfacts_project/facts/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+# from django.http import HttpResponseRedirect
+from .models import Fact
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    all_facts = Fact.objects.all()
+    context = {
+        'facts':all_facts,
+    }
+    return render(request, 'home.html', context)
     # return HttpResponse("Start")
+
+def addFact(request):
+    new_fact = Fact()
+    new_fact.text = request.POST.get('new_fact')
+    new_fact.user_id = request.user.id
+    new_fact.save()
+
+    return redirect("/")
+    
+
+
