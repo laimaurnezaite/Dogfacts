9ea01871a8591132906293a183a6ae610e4752c0
Laima Urnezaite <laima.urnezaite@gmail.com>
2020-05-27 22:26:09 +0300

    create test home page

--- dogfacts_project/facts/views.py
+++ dogfacts_project/facts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Start")
+    return render(request, 'home.html')
+    # return HttpResponse("Start")
