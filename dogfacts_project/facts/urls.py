28ceb3b47706a1cddb7fb0363614c24d92b857ef
Laima Urnezaite <laima.urnezaite@gmail.com>
2020-05-27 23:20:43 +0300

    add addFact url

--- dogfacts_project/facts/urls.py
+++ dogfacts_project/facts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, addFact
 
 urlpatterns = [
     path('', home, name='homepage'),
+    path('add', addFact, name='addFact')
 ]
