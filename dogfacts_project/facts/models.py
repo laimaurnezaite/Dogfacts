dc1d44cb379696af5dbb9544726068f83fecccf3
Laima Urnezaite <laima.urnezaite@gmail.com>
2020-05-27 22:27:00 +0300

    create Fact model and make migrations

--- dogfacts_project/facts/models.py
+++ dogfacts_project/facts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+class Fact(models.Model):
+    text = models.TextField()
+    DOG = "Dog"
+    types = [(DOG, "dog"),]
+    type = models.CharField(choices=types, default=DOG, max_length=255)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.id)
